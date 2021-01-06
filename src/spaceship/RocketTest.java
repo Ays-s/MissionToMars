b677da573d33915bc78cba2c5a54680957779cd3
Marie-Amelie-Defresne <marie-amelie.defresne@ensta-bretagne.org>
2021-01-06 19:51:01 +0100

    Ajout des fichiers de tests

---
+++ src/spaceship/RocketTest.java
@@ -0,0 +1,38 @@
+package spaceship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RocketTest {
+
+    @Test
+    void canCarry() {
+        U1 rocket = new U1();
+        Item item1 = new Item("Item 1", 2, 0, 0);
+        Item item2 = new Item("Item 2", 10, 0, 0);
+
+        boolean answer1 = rocket.canCarry(item1);
+        assertTrue(answer1);
+
+        boolean answer2 = rocket.canCarry(item2);
+        assertFalse(answer2);
+    }
+
+
+    @Test
+    void carry(){
+        U1 rocket = new U1();
+        Item item1 = new Item("Item 1", 2, 0, 0);
+        Item item2 = new Item("Item 2", 5,0 , 0);
+
+        rocket.carry(item1);
+        assertEquals(12, rocket.getWeight());
+
+        rocket.carry(item2);
+        assertEquals(17, rocket.getWeight());
+    }
+
+}
+
+
---
+++ src/spaceship/U1Test.java
@@ -0,0 +1,70 @@
+package spaceship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class U1Test {
+
+    @Test
+    void getCost() {
+        U1 rocket = new U1();
+        assertEquals(100, rocket.getCost());
+    }
+
+    @Test
+    void launch() {
+        int succesWithItem;
+        int nbrLoop = 10000;
+
+        System.out.println("Test launch() U1:");
+        for (int weight = 0; weight <= 8; weight++) {
+            Item item = new Item("Item 2", weight, 0, 0);
+            succesWithItem = 0;
+            for (int i = 0; i < nbrLoop; i++) {
+                U1 rocket = new U1();
+                rocket.carry(item);
+                if (rocket.launch()) {
+                    succesWithItem += 1;
+                }
+            }
+            System.out.println("Résultats avec un poids supplémentaire de : " + weight + "\n" + succesWithItem + " - rate : " + (float) succesWithItem / nbrLoop);
+
+            assertNotEquals(0, succesWithItem);
+            assertNotEquals(nbrLoop, succesWithItem);
+            boolean answer1 = (succesWithItem > nbrLoop);
+            assertFalse(answer1);
+            boolean answer2 = (succesWithItem < 0);
+            assertFalse(answer2);
+        }
+
+    }
+
+    @Test
+    void land() {
+        int succesWithItem;
+        int nbrLoop = 10000;
+
+        System.out.println("\nTest land() U1:");
+        for (int weight = 0; weight <= 8; weight++) {
+            Item item = new Item("Item 2", weight,0 ,0);
+            succesWithItem = 0;
+            for (int i = 0; i < nbrLoop; i++) {
+                U1 rocket = new U1();
+                rocket.carry(item);
+                if (rocket.land()) {
+                    succesWithItem += 1;
+                }
+            }
+            System.out.println("Résultats avec un poids supplémentaire de : " + weight + "\n" + succesWithItem + " - rate : " + (float) succesWithItem / nbrLoop);
+
+            assertNotEquals(0, succesWithItem);
+            assertNotEquals(nbrLoop, succesWithItem);
+            boolean answer1 = (succesWithItem > nbrLoop);
+            assertFalse(answer1);
+            boolean answer2 = (succesWithItem < 0);
+            assertFalse(answer2);
+        }
+
+    }
+}
